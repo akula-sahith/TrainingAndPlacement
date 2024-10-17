f56c6e4eab4646f04a6e3c4ce5e5350ff4862b39
Sahith Akula <akulasahith268@gmail.com>
2024-10-17 14:32:47 +0530

    Array was added

---
+++ Codes/CodingQuestions/RotateArray.java
@@ -0,0 +1,9 @@
+package Codes.CodingQuestions;
+import java.util.*;
+public class RotateArray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
---
+++ Codes/DSA/Java/Print2d.java
@@ -0,0 +1,18 @@
+package Codes.DSA.Java;
+
+public class Print2d {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = i + 1;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
